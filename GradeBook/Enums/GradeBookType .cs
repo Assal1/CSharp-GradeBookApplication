61dc4361b189e7eec2bfb031b951fa4603aedfce
Assal1 <49134353+Assal1@users.noreply.github.com>
2020-07-02 14:18:38 +0100

    Completed module

--- GradeBook/Enums/GradeBookType .cs
+++ GradeBook/Enums/GradeBookType .cs
@@ -6,10 +6,10 @@ namespace GradeBook.Enums
 {
     public enum GradeBookType
     {
-        Standard=0,
-        Ranked=1,
-        ESNU=2,
-        OneToFour=3,
-        SixPoint=4
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
     }
 }
--- GradeBook/GradeBooks/StandardGradeBook .cs
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name):base(name)
         {
-            Type = GradeBookType.Ranked;
+            Type = GradeBookType.Standard;
 
         }
     }
